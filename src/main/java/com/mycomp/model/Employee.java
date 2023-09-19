eaeaf1dfce37525a1ccab13799ce7e646b138041
Erkan <ErkanOzulu@users.noreply.github.com>
2023-09-19 23:41:04 +0300

    added validation annotation

---
+++ src/main/java/com/mycomp/model/Employee.java
@@ -0,0 +1,38 @@
+package com.mycomp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.time.LocalDate;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Employee {
+
+//comes from validation dependency
+
+    //     @NotNull            // Except null, anything can be accepted
+    //     @NotEmpty           // Except null and empty String anything can be accepted
+    //     @NotBlank           // Except null, empty String and only space anything can be accepted.
+
+    @NotBlank
+    @Size(max = 12, min = 2)
+    private String firstName;
+    private String lastName;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")// we have to define date time format otherwise thymeleaf gives error
+    private LocalDate birthday;
+    private String email;
+    private String password;
+    private String address;
+    private String address2;
+    private String city;
+    private String state;
+    private String zipCode;
+}
